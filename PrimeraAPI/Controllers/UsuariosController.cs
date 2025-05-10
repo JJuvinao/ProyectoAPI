c0f3154bc0ee812da8ac1dee10a194777d13ed54
niño <jjuvinao@unicesar.edu.co>
2025-05-10 18:43:32 -0500

    validacion

--- PrimeraAPI/Controllers/UsuariosController.cs
+++ PrimeraAPI/Controllers/UsuariosController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PrimeraAPI.Models;
+using PrimeraAPI.ObjectDto;
 
 namespace PrimeraAPI.Controllers
 {
@@ -39,6 +40,20 @@ namespace PrimeraAPI.Controllers
             }
 
             return usuario;
+        }
+
+        [HttpGet("GetUsername/{nombre}")]
+        public async Task<ActionResult> GetUsername(string nombre)
+        {
+            try
+            {
+                var existeUsuario = await _context.Usuarios.AnyAsync(u => u.Nombre == nombre);
+                return existeUsuario ? Conflict() : Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
         }
 
         // PUT: api/Usuarios/5
@@ -77,10 +92,52 @@ namespace PrimeraAPI.Controllers
         [HttpPost]
         public async Task<ActionResult<Usuario>> PostUsuario(Usuario usuario)
         {
-            _context.Usuarios.Add(usuario);
-            await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetUsuario", new { id = usuario.Id }, usuario);
+                var existeUsuario = await _context.Usuarios.AnyAsync(u => u.Nombre == usuario.Nombre);
+                if (existeUsuario)
+                {
+                    return Conflict("Ya existe un usuario con ese nombre.");
+                }
+
+                usuario.Contrasena = BCrypt.Net.BCrypt.HashPassword(usuario.Contrasena);
+
+                try
+                {
+                    _context.Usuarios.Add(usuario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException)
+                {
+                    return StatusCode(500, "Error al guardar el usuario en la base de datos.");
+                }
+
+                var usuarioDto = new UsuarioDto
+                {
+                    Id = usuario.Id,
+                    Nombre = usuario.Nombre,
+                    Rol = usuario.Rol,
+                    Correo = usuario.Correo
+                };
+
+                return CreatedAtAction("GetUsuario", new { id = usuario.Id }, usuarioDto);
+            }
+
+        [HttpPost ("login")]
+        public async Task<ActionResult<Usuario>> Login([FromBody] Usuario usuario)
+        {
+            var user = await _context.Usuarios
+         .FirstOrDefaultAsync(u => u.Nombre == usuario.Nombre);
+
+            if (user == null)
+            {
+                return Unauthorized("Usuario no encontrado");
+            }
+            if (!BCrypt.Net.BCrypt.Verify(usuario.Contrasena, user.Contrasena))
+            {
+                return Unauthorized("Contraseña incorrecta");
+            }
+
+            return Ok(new UsuarioDto(user.Id,user.Nombre,user.Rol,user.Correo));
         }
 
         // DELETE: api/Usuarios/5
--- PrimeraAPI/Models/Clase-Exam.cs
+++ PrimeraAPI/Models/Clase-Exam.cs
@@ -2,6 +2,7 @@
 {
     public class Clase_Exam
     {
+        public int Id { get; set; }
         public int id_clase { get; set; }
         public int id_examen { get; set; }
         public DateTime fecha_creacion { get; set; } = DateTime.Now;
