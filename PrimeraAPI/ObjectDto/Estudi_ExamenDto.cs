214f7dea5c3616b226a1eafcfefca2d5c4f9f6b4
Aldemarfino <aldemarje73@gmail.com>
2025-05-30 04:12:02 -0500

    Tocó ir paya entonces

--- PrimeraAPI/ObjectDto/Estudi_ExamenDto.cs
+++ PrimeraAPI/ObjectDto/Estudi_ExamenDto.cs
@@ -16,4 +16,15 @@
         public float? Nota { get; set; }
         public string? Recomendacion { get; set; }
     }
+
+    public class Estu_ExamPut
+    {
+        public int? Id_Examen { get; set; }
+        public int? Puntaje { get; set; }
+        public int? Aciertos { get; set; }
+        public int? Fallos { get; set; }
+        public DateTime? Tiempo { get; set; }
+        public float? Nota { get; set; }
+        public string? Recomendacion { get; set; }
+    }
 }
