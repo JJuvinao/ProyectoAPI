55ce4a835cc1c678b287c4a4470d0d116a9b8b95
niño <jjuvinao@unicesar.edu.co>
2025-05-30 06:06:43 -0500

    mmm

--- PrimeraAPI/Migrations/20250530104111_cambios2.Designer.cs
+++ PrimeraAPI/Migrations/20250530104111_cambios2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PrimeraAPI.Models;
 
@@ -11,9 +12,11 @@ using PrimeraAPI.Models;
 namespace PrimeraAPI.Migrations
 {
     [DbContext(typeof(ContextDB))]
-    partial class ContextDBModelSnapshot : ModelSnapshot
+    [Migration("20250530104111_cambios2")]
+    partial class cambios2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- PrimeraAPI/ObjectDto/Estudi_ExamenDto.cs
+++ PrimeraAPI/ObjectDto/Estudi_ExamenDto.cs
@@ -8,13 +8,12 @@
 
     public class Estu_ExamFrom
     {
-        public int Id { get; set; }
-        public int? Puntaje { get; set; }
+        public int? Id_Estudiane { get; set; }
+        public int? Id_Examen { get; set; }
+
+        public int? Intentos { get; set; }
         public int? Aciertos { get; set; }
         public int? Fallos { get; set; }
-        public DateTime? Tiempo { get; set; }
-        public float? Nota { get; set; }
-        public string? Recomendacion { get; set; }
     }
 
     public class Estu_ExamPut
